33ac9554180118a71959880cc99533fa6015088d
Pikay2908 <phamdacphustg@gmail.com>
2026-04-13 23:52:58 +0700

    update

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,9 +1,9 @@
 <div class="topbar reveal-up">
     <div class="container topbar-inner">
         <div class="topbar-left">
-            <a href="{{ url('/') }}" class="topbar-brand" aria-label="MediConnect">
+            {{-- <a href="{{ url('/') }}" class="topbar-brand" aria-label="MediConnect">
                 <img src="{{ asset('images/logo/MediConnect.png') }}" alt="MediConnect Logo">
-            </a>
+            </a> --}}
             <div class="topbar-item">
                 <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                     <path d="M4 5h16a2 2 0 0 1 2 2v.4l-10 6.25L2 7.4V7a2 2 0 0 1 2-2Zm18 4.75-9.47 5.92a1 1 0 0 1-1.06 0L2 9.75V17a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9.75Z"/>
